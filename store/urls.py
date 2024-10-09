1dddbb66892e7df30378473e2aaf07a5942659ad
Kasun Rathnayaka <k.rathnayaka365@gmail.com>
2024-10-09 16:07:28 +0530

    template base added, bug: template css stopped working

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.storehome, name='storehome'),
+    path('aboutus/',views.aboutus, name='aboutus'),
+    path('contactus/',views.contactus, name='contactus'),
 ]
--- store/views.py
+++ store/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def storehome(request):
     return render(request, 'storehome.html',{'title':'Store Home'})
+
+
+def aboutus(request):
+    return render(request, 'aboutus.html',{'title':'About Us'})
+
+
+def contactus(request):
+    return render(request, 'contactus.html',{'title':'Contact Us'})
