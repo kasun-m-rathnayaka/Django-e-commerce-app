b2eab67a841fe739db5f69aab8d3514157975671
Kasun Rathnayaka <k.rathnayaka365@gmail.com>
2024-10-09 13:56:30 +0530

    initial commit

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'playground.apps.PlaygroundConfig',
+    "store.apps.StoreConfig",
+    "phoneproject.apps.PhoneprojectConfig"
 ]
 
 MIDDLEWARE = [
---
+++ phoneproject/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PhoneprojectConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'phoneproject'
