f4df76ddfd8550247a17a22b4b39daa3dc8d5267
Kasun Rathnayaka <k.rathnayaka365@gmail.com>
2024-10-23 16:51:38 +0530

    configured templates

--- phoneproject/urls.py
+++ phoneproject/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
+
 ]
+
+urlpatterns += staticfiles_urlpatterns()
