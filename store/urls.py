cfe880e861200f6837f9f390477a14eadb671bc6
Kasun Rathnayaka <k.rathnayaka365@gmail.com>
2025-01-05 08:43:34 +0530

    refactor templates, changed url mapping

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.storehome , name='storehome'),
     path('aboutus', views.aboutus , name ='aboutus'),
+    path('reviews', views.reviews , name='reviews'),
     path('contactus', views.contactus , name ='contactus'),
 
 ]
--- store/views.py
+++ store/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def storehome(request):
-    return render(request,'store/storehome.html',{})
+    return render(request,'home.html',{})
 
 def aboutus(request):
     return render(request,'store/aboutus.html',{})
 
 def contactus(request):
     return render(request,'store/contactus.html',{})
+
+
+def reviews(request):
+    return render(request, 'store/reviews.html', {})
